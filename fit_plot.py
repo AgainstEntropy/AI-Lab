0bf89b972de342698ed931232d78b7fd75f4b581
PJLAB\wyh <wangyihao@pjlab.org.con>
2022-01-27 19:17:21 +0800

    almost working for every mat.

--- fit_plot.py
+++ fit_plot.py
@@ -8,50 +8,118 @@ import os
 
 import numpy as np
 import scipy.optimize
+from scipy.optimize import OptimizeWarning
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
 
 
 def gauss(x, A, mu, sigma, y0):
+    """
+    A gaussian function is like:
+        A * exp((x-mu)^2 / 2 * sigma^2) + y0
+    Args:
+        x (list(float)): some x points.
+        A (float): Amplitude.
+        mu (float): Mean.
+        sigma (float): Variance.
+        y0 (float): Bias.
+
+    Returns:
+
+    """
     return A * np.exp(-(x - mu) ** 2 / (2. * sigma ** 2)) + y0
 
 
-if __name__ == "__main__":
-    data_y = np.zeros((2976, 1))
-    data_y[:-1] = loadmat('./mats/268.mat')['val1']
-    data_y = data_y.reshape((-1, 4))
-    data_x = np.arange(0, len(data_y))
-    data_col = data_y[:, 3]
+def data_filter(data_x, data_col, col_idx, threshold=0.25, verbose=True):
+    """
 
-    # 筛选用于拟合的点集
+    Args:
+        data_x (list(int)): xs of raw data.
+        data_col (list(float)): ys of raw data. With the same length as data_x.
+        threshold (float): Set threshold for filter out values away from gaussian peak.
+
+    Returns:
+        x_fit, data_col_fit: data reserved for fitting directly.
+
+    """
+    # filter out zero value
     data_col_fit_idx = data_col != 0.
-    data_col_fit = data_col[data_col_fit_idx]
     x_fit = data_x[data_col_fit_idx]
-    print(x_fit.shape)
-    data_col_fit_idx = data_col_fit > min(data_col_fit) + (max(data_col_fit) - min(data_col_fit)) / 4
+    data_col_fit = data_col[data_col_fit_idx]
+
+    # filter out values away from gaussian peak
+    data_col_fit_idx = \
+        data_col_fit > min(data_col_fit) + (max(data_col_fit) - min(data_col_fit)) * threshold
     data_col_fit = data_col_fit[data_col_fit_idx]
     x_fit = x_fit[data_col_fit_idx]
-    print(x_fit.shape)
 
-    # 拟合初值
-    p0 = np.array([1., 300., 20, 0.1])
-    plt.scatter(x_fit, data_col_fit, label='origin data (used for fitting)', c='none', marker='o', s=5, edgecolors='C0')
-    plt.plot(x_fit, gauss(x_fit, *p0), label='initial fit curve', c='C3')
+    if verbose:
+        print(f"number of points used for fitting column {col_idx}: {len(x_fit)}")
+
+    if len(x_fit) < 55:
+        raise ValueError
+
+    return x_fit, data_col_fit
+
+
+def plot(x, ys, col_idx):
+    """
+
+    Args:
+        x (list(int)): xs of raw data.
+        ys (dict):
+            ys["raw"] (list(float)): ys of raw data.
+            ys["fit"] (list(float)): ys of fitted data.
+        col_idx (int): index of column.
+
+    Returns:
+        None. Plot directly.
+    """
+
+    plt.scatter(x, ys["raw"], label=f'origin data {col_idx}(used for fitting)',
+                c='none', marker='o', s=5, edgecolors=f'C{col_idx}')
+    plt.plot(x, ys["fit"], label=f'fit curve {col_idx}', c=f'C{col_idx}', ls='--')
+
     plt.xlim([100, 600])
     plt.ylim([0, 1])
-    plt.legend()
+    plt.legend(loc='best')
     plt.show()
 
-    # 拟合
-    popt, pcov = scipy.optimize.curve_fit(f=gauss, xdata=x_fit, ydata=data_col_fit, p0=p0)
-    print('Paras:\n', popt)  # 拟合结果参数值
-    print('Cov:\n', pcov)  # 拟合结果协方差
 
-    plt.plot(data_x, gauss(data_x, *popt), c='C3', label='fit curve')
+if __name__ == "__main__":
+    # data pre-process
+    file_path = './mats/149.mat'
+    data_y = np.zeros((2976, 1))
+    data_y[:-1] = loadmat(file_path)['val1']
+    data_y = data_y.reshape((-1, 4))
+    data_x = np.arange(0, len(data_y))
+
+    # fit and plot
+    para_names = ['A', 'mu', 'sigma', 'y0']
+    # 初值
+    p0 = np.array([0.5, 300., 20, 0.1])
     for i in range(4):
-        # plt.plot(data_x, data_y[:, i], label=f'origin data {i}', ls='--')
-        plt.scatter(data_x, data_y[:, i], label=f'origin data {i}', c='none', marker='o', s=5, edgecolors=f'C{i}')
-    plt.xlim([100, 600])
-    plt.ylim([0, 1])
-    plt.legend()
-    plt.show()
+        data_col = data_y[:, i]
+
+        # 筛选用于拟合的点集
+        try:
+            x_fit, data_col_fit = data_filter(data_x, data_col, i, threshold=0.25)
+        except ValueError:
+            print("*****数据过少,无法用于拟合*****\n")
+            continue
+
+        # 拟合
+        plot(x_fit, {"raw": data_col_fit, "fit": gauss(x_fit, *p0)}, col_idx=i)
+
+        # 拟合
+        try:
+            popt, pcov = scipy.optimize.curve_fit(f=gauss, xdata=x_fit, ydata=data_col_fit, p0=p0)
+            # 拟合结果参数值
+            for name, para in zip(para_names, popt):
+                print(f"{name}:\t{para:.4f}")
+            plot(data_x, {"raw": data_col, "fit": gauss(data_x, *popt)}, col_idx=i)
+        except (OptimizeWarning, RuntimeError):
+            # TODO: OptimizeWarning...
+            print("**********拟合失败**********\n")
+
+        print()
